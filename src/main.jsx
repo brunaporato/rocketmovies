2cb86478a4c9f5b5db1d22371cf27848b484d897
Bruna Porato <brunaporato@gmail.com>
2023-06-19 22:23:56 -0300

    users login

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import theme from './styles/theme'
 import { ThemeProvider } from 'styled-components'
 import { Routes } from './routes'
 
+import { AuthProvider } from './hooks/auth'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CreateGlobalStyle />
-        <Routes />
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,21 +1,49 @@
-import { Container, Form, Background } from "./styles";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
+import { Container, Form, Background } from "./styles";
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
+import { useAuth } from "../../hooks/auth";
 
 import { BiMailSend, BiLockAlt } from "react-icons/bi";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
+
   return (
     <Container>
       <Form>
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assistir</p>
         <h2>Faça seu login</h2>
-        <Input type="text" placeholder="Email" icon={BiMailSend} />
-        <Input type="password" placeholder="Senha" icon={BiLockAlt} />
-        <Button title="Entrar" isActive />
+
+        <Input
+          type="text"
+          placeholder="Email"
+          icon={BiMailSend}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <Input
+          type="password"
+          placeholder="Senha"
+          icon={BiLockAlt}
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <Button
+          title="Entrar"
+          isActive
+          onClick={handleSignIn}
+        />
         <Link className="link" to="/register">Criar conta</Link>
       </Form>
       <Background />
