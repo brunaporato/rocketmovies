fa29876558d254299e10841b753306cbc0c4e544
Bruna Porato <brunaporato@gmail.com>
2023-06-23 15:48:04 -0300

    add fetch notes function

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,18 +5,35 @@ import { Link } from "react-router-dom";
 
 import userNullAvatar from '../../assets/user-nullAvatar.jpg'
 import { api } from '../../services/api'
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 export function Header() {
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState([]);
+
   const { signOut, user } = useAuth();
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : userNullAvatar;
 
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(`/movienotes?title=${search}`);
+      setNotes(response.data);
+    }
+
+    fetchNotes();
+  }, [search]);
 
   return (
     <Container>
       <h1>RocketMovies</h1>
-      <input type="text" placeholder="Pesquisar pelo título" />
+      <input
+        type="text"
+        placeholder="Pesquisar pelo título"
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <User>
         <div>
           <h2>{user.name}</h2>
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -5,12 +5,57 @@ import { Input } from "../../components/Input"
 import { TagItem } from "../../components/TagItem"
 import { Button } from "../../components/Button"
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
-import { BiLeftArrowAlt } from 'react-icons/bi'
+import { BiLeftArrowAlt } from "react-icons/bi";
+import { useState } from "react";
+import { api } from "../../services/api";
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [rate, setRate] = useState("");
+
+  const [tag, setTag] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleAddTag() {
+    setTag(prevState => [...prevState, newTag]);
+    setNewTag("");
+  }
+
+  function handleDeleteTag(deleted) {
+    setTag(prevState => prevState.filter(tag => tag !== deleted))
+  }
+
+  async function handleNewNote() {
+    if(!title) {
+      return alert("Please enter the movie title.")
+    }
+
+    if(isNaN(Number(rate)) || rate > 5 || rate < 0) {
+      return alert("Rate this movie from 0 to 5")
+    }
+
+    if(newTag) {
+      return alert("There's a value in tag that wasn't added. Please add or delete it.")
+    }
+
+
+    await api.post("/movienotes", {
+      title,
+      description,
+      rating: rate,
+      tags: tag
+    });
+
+    alert("Movie note created");
+    navigate("/");
+  }
+
   return(
     <Container>
       <Header />
@@ -18,20 +63,52 @@ export function New() {
       <Link to="/"> <BiLeftArrowAlt /> Voltar</Link>
         <h1>Novo filme</h1>
         <div className="name-rate">
-          <Input type="text" placeholder="Título" />
-          <Input type="numer" placeholder="Nota (de 0 a 5)" />
+          <Input
+            type="text"
+            placeholder="Título"
+            onChange={e => setTitle(e.target.value)}
+          />
+
+          <Input
+            type="text"
+            placeholder="Nota (de 0 a 5)"
+            onChange={e => setRate(e.target.value)}
+          />
         </div>
-        <textarea placeholder="Observações" />
+
+        <textarea
+          placeholder="Observações"
+          onChange={e => setDescription(e.target.value)}
+          />
+
       <div className="tags">
         <p>Marcadores</p>
         <TagWrapper>
-          <TagItem value="new" />
-          <TagItem isNew placeholder="Nova tag" />
+          {
+            tag.map((tag, index) => (
+              <TagItem
+                key={String(index)}
+                value={tag}
+                onClick={() => handleDeleteTag(tag)}
+              />
+            ))
+          }
+          <TagItem
+            isNew
+            placeholder="Add Tag"
+            value={newTag}
+            onChange={e => setNewTag(e.target.value)}
+            onClick={handleAddTag}
+          />
         </TagWrapper>
       </div>
       <footer>
         <Button title="Excluir filme" />
-        <Button title="Salvar alterações" isActive />
+        <Button
+          isActive
+          title="Salvar alterações"
+          onClick={handleNewNote}
+        />
       </footer>
       </div>
     </Container>
